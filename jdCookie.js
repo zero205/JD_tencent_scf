2282f8defb71a45e198295938c4168e40665a799
1727026248 <83265746+1727026248@users.noreply.github.com>
2021-05-21 13:12:54 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -3,7 +3,7 @@
  */
 //此处填写京东账号cookie。
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJgpz8KADB2OEdhxixtBjkXDDAkJEXGjtchTZ_i09b9jUEJkW0pv-qdgFqnwaVQ-tfy18IBUFI; pt_pin=18780479218_p;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断环境变量里面是否有京东ck
