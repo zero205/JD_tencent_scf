614fa1363ba2fc7c8a18f18690a1fdc665faab54
zero205 <1184642956@qq.com>
2021-06-19 22:21:16 +0800

    fix

--- jd_jxlhb.js
+++ jd_jxlhb.js
@@ -66,15 +66,15 @@ const BASE_URL = 'https://wq.jd.com/cubeactive/steprewardv3'
     $.canHelp = true;
     $.max = false;
     $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
-    // for (let code of $.packetIdArr) {
-    //   if (!code) continue;
-    //   if ($.UserName === code['userName']) continue;
-    //   if (!$.canHelp) break
-    //   if ($.max) break
-    //   console.log(`【${$.UserName}】去助力【${code['userName']}】邀请码：${code['strUserPin']}`);
-    //   await enrollFriend(code['strUserPin']);
-    //   await $.wait(2500);
-    // }
+     for (let code of $.packetIdArr) {
+       if (!code) continue;
+       if ($.UserName === code['userName']) continue;
+       if (!$.canHelp) break
+       if ($.max) break
+       console.log(`【${$.UserName}】去助力【${code['userName']}】邀请码：${code['strUserPin']}`);
+       await enrollFriend(code['strUserPin']);
+       await $.wait(2500);
+     }
     if ($.canHelp) {
       console.log(`\n【${$.UserName}】有剩余助力机会，开始助力作者\n`)
       for (let item of $.authorMyShareIds) {
