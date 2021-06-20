25e87c9d79cc9e39a3040176f6285d95ebdb7fc7
jkluio78 <74967641+jkluio78@users.noreply.github.com>
2021-06-20 16:20:04 +0800

    Update tencentscf.js

--- tencentscf.js
+++ tencentscf.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const yaml = require("js-yaml");
 
 process.env.action = 0;
+const MemorySize=process.env.TENCENT_MemorySize?(Number(process.env.TENCENT_MemorySize)>64?128:64):64;
 const ScfClient = tencentcloud.scf.v20180416.Client;
 const clientConfig = {
   credential: {
@@ -56,7 +57,7 @@ const sleep = ms => new Promise(res => setTimeout(res, ms));
         },
         FunctionName: process.env.TENCENT_FUNCTION_NAME,
         Runtime: "Nodejs12.16",
-        MemorySize: 64,
+        MemorySize: MemorySize,
         Timeout: 86400,
         AsyncRunEnable: "true",
         Environment: {
