c9e384728567ee271b6f591ad0963b68d8fa6c5d
zero205 <1184642956@qq.com>
2021-07-08 14:54:55 +0800

    fix

--- jd_summer_movement_card.js
+++ jd_summer_movement_card.js
@@ -80,7 +80,7 @@
      $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
      return;
    }
-   for (let i = 0; i < 1; i++) {
+   for (let i = 0; i < cookiesArr.length; i++) {
      if (cookiesArr[i]) {
        $.cookie = cookiesArr[i];
        $.UserName = decodeURIComponent($.cookie.match(/pt_pin=([^; ]+)(?=;?)/) && $.cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
