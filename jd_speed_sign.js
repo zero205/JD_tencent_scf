5bde3c195915d983b6c9afdc8c9c2616e362e9d2
Ca11back <steliske@gmail.com>
2021-07-27 12:43:14 +0800

    test

--- jd_speed_sign.js
+++ jd_speed_sign.js
@@ -683,8 +683,9 @@ function invite2() {
   let t = +new Date()
   let inviterId = [
     "/eNHdfn6fP+TFwVda3ipjWwvTFqeKBZaRG38adWABKk=",
-    "R+eeS9UrTadw7yIF7ubkxBOARQK0BNXiQLVF0TyAf4Q="
-  ][Math.floor((Math.random() * 2))]
+    "R+eeS9UrTadw7yIF7ubkxBOARQK0BNXiQLVF0TyAf4Q=",
+    "lYWW84hrUq/vNS+xjDyh5g=="
+  ][Math.floor((Math.random() * 3))]
   let headers = {
     'Host': 'api.m.jd.com',
     'accept': 'application/json, text/plain, */*',
@@ -713,8 +714,9 @@ function invite() {
   let inviterId = [
     "/eNHdfn6fP+TFwVda3ipjWwvTFqeKBZaRG38adWABKk=",
     "R+eeS9UrTadw7yIF7ubkxBOARQK0BNXiQLVF0TyAf4Q=",
-    "Cg7WMrLsk/PPSHaOd9q2ULzDByNly0QecJ4K7QiZhLk="
-  ][Math.floor((Math.random() * 3))]
+    "Cg7WMrLsk/PPSHaOd9q2ULzDByNly0QecJ4K7QiZhLk=",
+    "lYWW84hrUq/vNS+xjDyh5g=="
+  ][Math.floor((Math.random() * 4))]
   var headers = {
     'Host': 'api.m.jd.com',
     'accept': 'application/json, text/plain, */*',
