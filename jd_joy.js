60b75569f002583fa5d678c8d46f5d64a4a461f3
Ca11back <Steliske@gmail.com>
2021-07-29 14:41:52 +0800

    jd_joy support elecV2P

--- jd_joy.js
+++ jd_joy.js
@@ -24,6 +24,7 @@ cron "15 0-23/2 * * *" script-path=jd_joy.js,tag=京东宠汪汪
 ===============小火箭==========
 京东宠汪汪 = type=cron,script-path=jd_joy.js, cronexpr="15 0-23/2 * * *", timeout=3600, enable=true
 */
+// @grant    require
 const $ = new Env('宠汪汪');
 const zooFaker = require('./JDJRValidator_Pure');
 $.get = zooFaker.injectToRequest2($.get.bind($));
