de3b26b0044c278db95a4545622cea829a536862
Ca11back <Steliske@gmail.com>
2021-08-15 22:32:27 +0800

    Update jd_bean_sign.js

--- jd_bean_sign.js
+++ jd_bean_sign.js
@@ -95,7 +95,7 @@ if ($.isNode()) {
 
 function changeFile (content,cookie) {
   console.log(`开始替换变量`)
-  let newContent = content.replace(/var OtherKey = '.*'/, `var OtherKey = '${cookie}'`);
+  let newContent = content.replace(/var OtherKey = `.*`/, `var OtherKey = '${cookie}'`);
   // newContent = newContent.replace(/const NodeSet = 'CookieSet.json'/, `const NodeSet = '${NodeSet}'`)
   if (process.env.JD_BEAN_STOP && process.env.JD_BEAN_STOP !== '0') {
     newContent = newContent.replace(/var stop = '0'/, `var stop = '${process.env.JD_BEAN_STOP}'`)
