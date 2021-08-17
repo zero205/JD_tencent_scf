fcdd34dd82e9c8a35f6fade6bf2ff62e289063c0
Ca11back <Steliske@gmail.com>
2021-08-17 16:13:21 +0800

    Update jd_joy_run.js

--- jd_joy_run.js
+++ jd_joy_run.js
@@ -51,10 +51,10 @@ const JD_BASE_API = `https://draw.jdfcloud.com//pet`;
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : {};
 //下面给出好友邀请助力的示例填写规则
-let invite_pins = ['jd_5bb16d6462414,jd_7399bee01a89e,jd_6e0cd5023b791,13536765947_p,jd_LyVyYIZjfyOm'];
-let run_pins = ['jd_5bb16d6462414,jd_7399bee01a89e,jd_6e0cd5023b791,13536765947_p,jd_LyVyYIZjfyOm'];
+let invite_pins = ['jd_5bb16d6462414,jd_7399bee01a89e,jd_6e0cd5023b791,13536765947_p,jd_LyVyYIZjfyOm,15022052312_p'];
+let run_pins = ['jd_5bb16d6462414,jd_7399bee01a89e,jd_6e0cd5023b791,13536765947_p,jd_LyVyYIZjfyOm,15022052312_p'];
 //friendsArr内置太多会导致IOS端部分软件重启,可PR过来(此处目的:帮别人助力可得30g狗粮)
-let friendsArr = ["jd_5bb16d6462414", "jd_7399bee01a89e", "jd_6e0cd5023b791", "13536765947_p", "jd_LyVyYIZjfyOm"]
+let friendsArr = ["jd_5bb16d6462414", "jd_7399bee01a89e", "jd_6e0cd5023b791", "13536765947_p", "jd_LyVyYIZjfyOm","15022052312_p"]
 
 
 //IOS等用户直接用NobyDa的jd cookie
