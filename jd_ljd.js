77ee26acbf90be3c8accf27d2d1f5e97104b93cc
zero205 <24651341+Ca11back@users.noreply.github.com>
2021-08-22 13:26:28 +0000

    Sync Upstream

--- jd_ljd.js
+++ jd_ljd.js
@@ -2,6 +2,7 @@
 /*
 // https://h5.m.jd.com/rn/42yjy8na6pFsq1cx9MJQ5aTgu3kX/index.html
 
+cron 8 0 * * * jd_ljd.js
 入口：首页-领京豆-升级赚京豆
 21 9 * * * https://raw.githubusercontent.com/smiek2221/scripts/master/gua_MMdou.js, tag=MM领京豆, enabled=true
 
