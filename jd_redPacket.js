be1071ff5884310210465b03442791f9e9c5a6f1
zero205 <24651341+Ca11back@users.noreply.github.com>
2021-09-04 12:29:33 +0000

    Sync Upstream

--- jd_redPacket.js
+++ jd_redPacket.js
@@ -47,15 +47,12 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     await $.wait(1000)
     res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/jd_red.json')
   }
-  if (res && res.length > 0) {
-    $.authorCode = [getRandomArrayElements(res, 1)[0]];
-  }
   let res2 = await getAuthorShareCode('https://raw.githubusercontent.com/zero205/updateTeam/main/shareCodes/jd_red.json')  
   if (!res2) {
     await $.wait(1000)
     res2 = await getAuthorShareCode('https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/jd_red.json')
   }
-  $.authorMyShareIds = [...($.authorCode || []),...(res2 || [])];
+  $.authorMyShareIds = [...(res && [res[Math.floor((Math.random() * res.length))]] || []),...(res2 || [])];
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
@@ -691,16 +688,6 @@ function TotalBean() {
   })
 }
 
-function getRandomArrayElements(arr, count) {
-  var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
-  while (i-- > min) {
-      index = Math.floor((i + 1) * Math.random());
-      temp = shuffled[index];
-      shuffled[index] = shuffled[i];
-      shuffled[i] = temp;
-  }
-  return shuffled.slice(min);
-}
 function jsonParse(str) {
   if (typeof str == "string") {
     try {
