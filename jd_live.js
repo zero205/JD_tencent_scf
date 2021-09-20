6d2dfeadc7091410dc7f89ada8c367f0a01a3165
zero205 <24651341+Ca11back@users.noreply.github.com>
2021-09-20 22:54:46 +0000

    Sync Upstream

--- jd_live.js
+++ jd_live.js
@@ -300,7 +300,7 @@ function getSign(functionid, body, uuid) {
         Host,
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       },
-      timeout: 15000
+      timeout: 30 * 1000
     }
     $.post(options, (err, resp, data) => {
       try {
