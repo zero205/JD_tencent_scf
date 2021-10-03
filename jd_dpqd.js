8b9e59a5af2d8fc84ddd915e5d40fb34d526fd36
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-10-03 20:14:11 +0800

    Update jd_dpqd.js

--- jd_dpqd.js
+++ jd_dpqd.js
@@ -11,7 +11,7 @@ if (process.env.DPQDTK) {
   token = [...process.env.DPQDTK.split('&'),...token]
 }
 if (!token.length) {
-  console.log('无店铺签到token,不执行.需要请入群查看频道信息.')
+  console.log('无店铺签到token,不执行.需自备token:环境变DPQDTK: tk1&tk2. 详细说明请入群查看频道信息.')
 }
 const $ = new Env('店铺签到');
 
