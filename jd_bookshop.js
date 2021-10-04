a4825046ec0569380b256f52b1b1882e003479ed
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-10-04 10:40:28 +0800

    Update jd_bookshop.js

--- jd_bookshop.js
+++ jd_bookshop.js
@@ -2,7 +2,7 @@
 口袋书店
 更新时间：2021-06-26 
 加了一个码,修复需要手动打开的问题
-活动入口：京东app首页-京东图书-右侧口袋书店
+活动入口：https://lzdz-isv.isvjcloud.com/dingzhi/book/develop/activity?activityId=dz2010100034444201(感谢TG群友@gh2923提供地址)
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 ============Quantumultx===============
