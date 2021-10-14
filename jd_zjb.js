fe8c6437af5076ee7bfac262a385311199143681
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-10-14 10:13:37 +0800

    Update jd_zjb.js

--- jd_zjb.js
+++ jd_zjb.js
@@ -64,7 +64,6 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
         await help()
       } else {
         await help2("zero205","%2FeNHdfn6fP%2BTFwVda3ipjWwvTFqeKBZaRG38adWABKk%3D")
-        await help2("zero205","R%20eeS9UrTadw7yIF7ubkxBOARQK0BNXiQLVF0TyAf4Q%3D")
         await help2("whisper liu","Sev6JWjut6GyaEHJIWpSQQ%3D%3D")
         
       }
