12beb0b63c27227ec3fa1627b0715d265db87a8c
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-10-21 18:11:00 +0800

    Update jd_xsqjd.js

--- jd_xsqjd.js
+++ jd_xsqjd.js
@@ -1,6 +1,7 @@
 /*
 * 活动：APP - 京东超市 - 限时抢京豆
 * 第一个CK助力作者，其他CK助力第一个CK
+cron 23 7,9 * * * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_xsljd.js
 * */
 const $ = new Env('限时抢京豆');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
@@ -33,7 +34,7 @@ let autoCode = '';
     let res = [];
     try{res = await getAuthorShareCode('https://raw.githubusercontent.com/lsh26/share_code/main/shop.json');}catch (e) {}
     if(!res){
-        try{res = await getAuthorShareCode('https://raw.fastgit.org/lsh26/share_code/main/shop.json');}catch (e) {}
+        try{res = await getAuthorShareCode('https://gitee.com/star267/share-code/raw/master/shop.json');}catch (e) {}
         if(!res){res = [];}
     }
     let res2 = [];
@@ -54,8 +55,6 @@ let autoCode = '';
     if(JSON.stringify(ownCode) === '{}'){
         return ;
     }
-    console.log(JSON.stringify(codeList))
-    return ;
     if(cookiesArr.length>0){
         const promiseArr = cookiesArr.map((ck, index) => help(ck));
         await Promise.all(promiseArr);
