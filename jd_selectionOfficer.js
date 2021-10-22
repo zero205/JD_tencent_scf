a3778c7c0b3aee1d1462ce2c4da1ea400d2dcf3b
zero205 <24651341+Ca11back@users.noreply.github.com>
2021-10-22 03:38:28 +0000

    Sync Upstream

--- jd_selectionOfficer.js
+++ jd_selectionOfficer.js
@@ -298,7 +298,7 @@ async function takeGetRequest(type) {
             'Connection' : `keep-alive`,
             'Accept' : `application/json, text/plain, */*`,
             'User-Agent': $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
-            'Authorization':`Bearer ${$.accessToken ?? 'undefined'}`,
+            'Authorization':`Bearer ${$.accessToken || 'undefined'}`,
             'Referer' : `https://xinruimz1-isv.isvjcloud.com/loading/`,
             'Accept-Language':'zh-cn'
         },
