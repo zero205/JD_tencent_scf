7ca2c4ca8d903aec2476b4a38d3d16797b305333
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-10-26 16:03:28 +0800

    Update jd_kyd.js

--- jd_kyd.js
+++ jd_kyd.js
@@ -3,7 +3,6 @@
 ##惊喜欢乐砍 自定义变量 入口惊喜APP我的 惊喜欢乐砍
 export launchid="ba3b268758521b2a48ce7ed61b82ff7a" ##你的邀请码
 export first="false" ##第一次参加变量设置为true查看商品ID 填写商品ID后自动获取邀请码邀请  如果只助力 变量设置为false
-export active="" ##商品ID
 
 [task_local]
 #柠檬是兄弟就砍我2
@@ -18,13 +17,13 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let cookiesArr = [],
     cookie = '',
     message;
-const active = '';
+// const active = '';
 let first = false; //第一次参加变量设置为true查看商品ID 填写商品ID后自动获取邀请码邀请  如果只助力 变量设置为false
 let launchid
 
-if (process.env.active) {
-    active = process.env.active;
-}
+// if (process.env.active) {
+//     active = process.env.active;
+// }
 
 if (process.env.first) {
     first = true;
