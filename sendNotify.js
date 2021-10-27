16c450f8c9f5752c284e85d5919cd824b4bd4816
Svenshen <zhenhualoveying@gmail.com>
2021-10-27 15:01:43 +0800

    Update sendNotify.js
    update gobot push

--- sendNotify.js
+++ sendNotify.js
@@ -193,10 +193,10 @@ function gobotNotify(text, desp, time = 2100) {
   return new Promise((resolve) => {
     if (GOBOT_URL) {
       const options = {
-        url: `${GOBOT_URL}?access_token=${GOBOT_TOKEN}&${GOBOT_QQ}`,
-        json: { message: `${text}\n${desp}` },
-        headers: {
-          'Content-Type': 'application/json',
+        url: `${GOBOT_URL}`,
+        json: { 
+          message: `${text}\n${desp}` ,
+          user_id:`${GOBOT_QQ}`
         },
         timeout,
       };
