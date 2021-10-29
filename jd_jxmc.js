1e2cc9127b03958dcba2ed1089dd0b1277eceaa6
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-10-29 14:19:45 +0800

    fix

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -38,12 +38,12 @@ if ($.isNode()) {
 }
 !(async () => {
     $.CryptoJS = $.isNode() ? require('crypto-js') : CryptoJS;
+    $.fingerprint = '';$.token = '';
     await requestAlgo();
     if (!cookiesArr[0]) {
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
         return;
     }
-    $.fingerprint = '';$.token = '';
     for (let i = 0; i < cookiesArr.length; i++) {
         $.index = i + 1;
         $.cookie = cookiesArr[i];
