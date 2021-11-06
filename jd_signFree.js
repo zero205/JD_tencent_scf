6d9755104741f77fd55debe60396868b14203174
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-11-06 10:29:59 +0800

    Update jd_signFree.js

--- jd_signFree.js
+++ jd_signFree.js
@@ -88,9 +88,10 @@ function query() {
                     $.signFreeOrderInfoList = data.data.signFreeOrderInfoList
                     if (data.success == true) {
                         if (data.data.risk == true) {
-                            console.log("风控用户,跳过");
-                            msg.push("风控用户,跳过")
-                        }else if (!data.data.signFreeOrderInfoList) {
+                            console.log("风控用户,可能有异常");
+                            msg.push("风控用户,可能有异常")
+                        }
+                        if (!data.data.signFreeOrderInfoList) {
                             console.log("没有需要签到的商品,请到京东极速版[签到免单]购买商品");
                             msg.push("没有需要签到的商品,请到京东极速版[签到免单]购买商品")
                         } else {
