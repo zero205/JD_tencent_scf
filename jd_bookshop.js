cf4f7937d86e578d7333ebfc75d7372b8803e4d8
liuranya <52848742+liuranya@users.noreply.github.com>
2021-11-07 20:17:46 +0800

    口袋书店

--- jd_bookshop.js
+++ jd_bookshop.js
@@ -29,7 +29,7 @@ let ADD_CART = false
 ADD_CART = $.isNode() ? (process.env.PURCHASE_SHOPS ? process.env.PURCHASE_SHOPS : ADD_CART) : ($.getdata("ADD_CART") ? $.getdata("ADD_CART") : ADD_CART);
 // 加入购物车开关，与东东小窝共享
 
-let inviteCodes = ['']
+let inviteCodes = ['c1161bed3537492fb86b5e83d3daf421']
 
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
