7141c57b0dffac81ce135be65e67cc8f8d0fd469
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-11-11 21:47:51 +0800

    increase delay

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -497,7 +497,7 @@ async function doMotion(petidList){
         console.log(`开始第${i + 1}次割草`);
         let mowingInfo = await takeRequest(`jxmc`,`operservice/Action`,`&type=2`,'activeid%2Cactivekey%2Cchannel%2Cjxmc_jstoken%2Cphoneid%2Csceneid%2Ctimestamp%2Ctype',true);
         console.log(`获得金币：${mowingInfo.addcoins || 0}`);
-        await $.wait(2000);
+        await $.wait(3000);
         if(Number(mowingInfo.addcoins) >0 ){
             runFlag = true;
         }else{
@@ -527,7 +527,7 @@ async function doMotion(petidList){
             runFlag = false;
             console.log(`未获得金币暂停割鸡腿`);
         }
-        await $.wait(2000);
+        await $.wait(3000);
     }
 }
 async function doTask(){
