6f9c27256d33b894b7676113bf385f620b329595
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-11-11 16:05:07 +0800

    add cron

--- jd_unsubscribe.js
+++ jd_unsubscribe.js
@@ -5,6 +5,7 @@
  * @LastEditors: X1a0He
  * @Description: 批量取关京东店铺和商品
  * @Fixed: 不再支持Qx，仅支持Node.js
+ cron 30 23 * * * https://raw.githubusercontent.com/zero205/JD_tencent_scf/main/jd_unsubscribe.js
  */
 const $ = new Env('批量取关店铺和商品');
 //Node.js用户请在jdCookie.js处填写京东ck;
