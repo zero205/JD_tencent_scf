3f0eff33df0d73a93e5fbffc3893a19a2391a571
liuranya <52848742+liuranya@users.noreply.github.com>
2021-11-12 16:17:51 +0800

    汪汪乐园每日任务

--- jd_joy_park_task.js
+++ jd_joy_park_task.js
@@ -1,7 +1,7 @@
 /*
 
 脚本默认会帮我助力开工位，介意请添加变量HELP_JOYPARK，false为不助力
-export HELP_JOYPARK=""
+export HELP_JOYPARK="false"
 
 更新地址：https://github.com/Tsukasa007/my_script
 ============Quantumultx===============
@@ -34,17 +34,7 @@ if ($.isNode()) {
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
 $.invitePinTaskList = []
-$.invitePin = [
-  "NZ42T3lD49qiTGpZsSucXRS8kD-wJ4K6FxwxUpNbb4I",
-  "zZkewfd3OKs-WtoJd8Jw6OIrD81WzO3SX56S2DGMlZ0",
-  "7zG4VHS99AUEoX1mQTkC9Q",
-  "BbsjCRrQudIL06kRvqmVln053h03GiApg7HN_Vhy_Og",
-  "sAxL-dc5T6lS6wtKqP6SlA",
-  "bcVxt4PbZdbX7tiT1Q_ubg",
-  "GLdMkFrZHXG8-YUnhakmEA",
-  "hwQ_gsL-AJeC0gGTlU0Z8w",
-  "DsYL6f_31DFANxBPMc00MA"
-]
+$.invitePin = []
 const JD_API_HOST = `https://api.m.jd.com/client.action`;
 message = ""
 !(async () => {
