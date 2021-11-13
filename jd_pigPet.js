20e8713cc521e4d32a1885ace50679a76798f217
liuranya <321082081@qq.com>
2021-11-13 20:30:35 +0800

    金融养猪

--- jd_pigPet.js
+++ jd_pigPet.js
@@ -867,7 +867,7 @@ function finishReadMission(missionId, readTime) {
 function getShareCode() {
   return new Promise(resolve => {
     $.get({
-      url: "http://152.136.20.87/code.json",
+      url: "https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/pig.json",
       headers: {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }
