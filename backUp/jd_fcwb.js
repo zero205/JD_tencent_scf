44082a84bfafe1e19ccf5011b43a41f430b6fe8c
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-11-14 10:11:59 +0800

    Update jd_fcwb.js

--- backUp/jd_fcwb.js
+++ backUp/jd_fcwb.js
@@ -4,7 +4,9 @@
     1、脚本只执行助力和做1个任务,需要手动进活动进行游戏
     2、第一个账号会助力作者，其他账号助力第一个CK
 cron 40 12,16 * * * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_fcwb.js
-
+支持助力码
+fcwbinviteCode
+fcwbinviter
 * * */
 const $ = new Env('发财挖宝');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
