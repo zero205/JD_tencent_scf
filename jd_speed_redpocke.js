6b3bc043b8fb026695f39891fbf9fb004fba76c3
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-11-16 19:39:02 +0800

    Update jd_speed_redpocke.js

--- jd_speed_redpocke.js
+++ jd_speed_redpocke.js
@@ -29,7 +29,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let cookiesArr = [], cookie = '', message;
-const linkIds = ["9wdf1YTT2L59Vr-meKskLA", "7ya6o83WSbNhrbYJqsMfFA"];
+const linkIds = ["7ya6o83WSbNhrbYJqsMfFA"];
 const signLinkId = '9WA12jYGulArzWS7vcrwhw';
 let linkId
 
