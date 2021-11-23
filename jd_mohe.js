6dd3e736feb325e1c6c66b2d5b5752fe640e3eda
zero205 <1184642956@qq.com>
2021-11-23 14:27:53 +0800

    Update jd_mohe.js
    助力火爆则跳过助力

--- jd_mohe.js
+++ jd_mohe.js
@@ -95,6 +95,9 @@ $.shareId = [];
       if (res && res['code'] === 2005) {
         console.log(`次数已用完，跳出助力`)
         break
+      } else if (res && res['code'] === 1002) {
+        console.log(`账号火爆，跳出助力`)
+        break
       }
     }
   }
