f73d34217a994c92923d30f4a56eae18b56f05f5
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-12-07 10:08:38 +0800

    Update jd_xiaolong.js

--- jd_xiaolong.js
+++ jd_xiaolong.js
@@ -142,7 +142,7 @@ function takePostRequest(type) {
             body = `activityId=${activityID}&actorUuid=${actorUuid}&param=${$.param}&taskType=${$.taskType}&pin=${encodeURIComponent(pin)}&shareUuid=${$.shareUuid}`;
             break;
         case 'doTask2':
-            let aa = ['29b379e745cf4463a08f2efc70fc27a4'];
+            let aa = ['29b379e745cf4463a08f2efc70fc27a4','06f4ada464f546fa9a787b72b98e65be'];
             let code = getRandomArrayElements(aa,1)[0]
             url = 'https://lzdz-isv.isvjcloud.com/unify/cardTaskDraw/doTask';
             body = `activityId=${activityID}&actorUuid=${actorUuid}&param=${$.param}&taskType=20&pin=${encodeURIComponent(pin)}&shareUuid=${code}`;
