6cb409390e247d3d358f7d39f1ee1aa92aae2f83
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-12-28 08:37:47 +0800

    Update jd_cfd.js

--- jd_cfd.js
+++ jd_cfd.js
@@ -174,11 +174,13 @@ async function cfd() {
     //小程序每日签到
     await $.wait(2000)
     await getTakeAggrPage('wxsign')
-
-    //使用道具
-    await $.wait(2000)
-    await GetPropCardCenterInfo()
-
+    
+    if (new Date().getHours() < 22){
+      //使用道具
+      await $.wait(2000)
+      await GetPropCardCenterInfo()
+    }
+    
     //助力奖励
     await $.wait(2000)
     await getTakeAggrPage('helpdraw')
