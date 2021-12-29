210e450ec0d4e08071431e4ca7a6e9f61257e340
zero205 <1184642956@qq.com>
2021-12-29 19:31:49 +0800

    Update jd_speed_sign.js

--- jd_speed_sign.js
+++ jd_speed_sign.js
@@ -829,10 +829,9 @@ function invite() {
     "/eNHdfn6fP+TFwVda3ipjWwvTFqeKBZaRG38adWABKk=",
     "R+eeS9UrTadw7yIF7ubkxBOARQK0BNXiQLVF0TyAf4Q=",
     "4OAzIinjvoFYHDImve1/VA==",
-    "N0xw0OCPJTUt8xzWZPWs3w==",
     "z///voP4pDo+KI13j5As9g==",
     "Sev6JWjut6GyaEHJIWpSQQ=="
-  ][Math.floor((Math.random() * 6))]
+  ][Math.floor((Math.random() * 5))]
   var headers = {
     'Host': 'api.m.jd.com',
     'accept': 'application/json, text/plain, */*',
