c71f48a5e7635c8597fdc1819bd4425ad9886772
Ca11back <24651341+Ca11back@users.noreply.github.com>
2022-01-03 15:30:19 +0800

    Update jd_m_sign.js

--- jd_m_sign.js
+++ jd_m_sign.js
@@ -65,7 +65,7 @@ async function jdsign() {
     console.log(`签到开始........`)
     await getInfo("https://pro.m.jd.com/mall/active/3S28janPLYmtFxypu37AYAGgivfp/index.html");//拍拍二手签到
     await $.wait(1000)
-    await getInfo("https://pro.m.jd.com/mall/active/3joSPpr7RgdHMbcuqoRQ8HbcPo9U/index.html");//生活特权签到
+    await getInfo("https://pro.m.jd.com/mall/active/4QjXVcRyTcRaLPaU6z2e3Sw1QzWE/index.html");//全城购签到
     await $.wait(1000)
     await getInfo("https://pro.m.jd.com/mall/active/kPM3Xedz1PBiGQjY4ZYGmeVvrts/index.html");//陪伴
 //     await getInfo("https://pro.m.jd.com/mall/active/ZrH7gGAcEkY2gH8wXqyAPoQgk6t/index.html");//箱包签到
