630da41c271a9047be74f95e390de79b6d53deba
Ca11back <24651341+Ca11back@users.noreply.github.com>
2022-01-16 11:42:21 +0800

    Update jd_tiger.js

--- jd_tiger.js
+++ jd_tiger.js
@@ -106,7 +106,7 @@ Object.keys(jdCookieNode).forEach((item) => {
         }
     }
     for (let i = 0; i < cookiesArr.length; i++) {
-        ccookie = cookiesArr[i]
+        cookie = cookiesArr[i]
         const userName = decodeURIComponent(cookie.match(/pt_pin=(.+?);/) && cookie.match(/pt_pin=(.+?);/)[1])
         console.log(`\n开始【京东账号${i + 1}】${userName}\n`)
 
