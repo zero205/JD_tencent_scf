19bc1a5fcb84fd9783caed403dc94e475e00e20d
Ca11back <24651341+Ca11back@users.noreply.github.com>
2022-02-06 22:21:34 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -199,7 +199,7 @@ async function sendNotify(
   text,
   desp,
   params = {'TG交流群':'https://t.me/jd_zero_205'},
-  author = '\n\n云函数老用户这个月开始实行新收费政策,请自行去自己私库页面或查看群内置顶.不需要请及时删除函数和github私库.',
+  author = '\n\n请回到仓库main分支或入群查看下新的拉库命令.',
 ) {
   let no_notify = process.env.no_notify
   if (no_notify) {
